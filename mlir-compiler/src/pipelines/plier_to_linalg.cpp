dc317a25262bc7987455a8901f82dff043d1038c
Butygin <ivan.butygin@intel.com>
2020-10-28 22:02:36 +0300

    move call lowering to separate file

--- mlir-compiler/src/pipelines/plier_to_linalg.cpp
+++ mlir-compiler/src/pipelines/plier_to_linalg.cpp
@@ -15,6 +15,7 @@
 #include "plier/dialect.hpp"
 
 #include "pipelines/plier_to_std.hpp"
+#include "rewrites/call_lowering.hpp"
 #include "rewrites/type_conversion.hpp"
 
 #include "base_pipeline.hpp"
@@ -80,70 +81,6 @@ mlir::Type map_plier_type(mlir::TypeConverter& converter, mlir::Type type)
     }
     return nullptr;
 }
-
-llvm::StringRef extract_bound_func_name(llvm::StringRef name)
-{
-    assert(!name.empty());
-    auto len = name.find(' ');
-    return name.substr(0, len);
-}
-
-struct CallOpLowering : public mlir::OpRewritePattern<plier::PyCallOp>
-{
-    using resolver_t = llvm::function_ref<mlir::LogicalResult(plier::PyCallOp, llvm::StringRef, llvm::ArrayRef<mlir::Value>, mlir::PatternRewriter&)>;
-
-    CallOpLowering(mlir::TypeConverter &/*typeConverter*/,
-                   mlir::MLIRContext *context,
-                   resolver_t resolver):
-        OpRewritePattern(context), resolver(resolver) {}
-
-    mlir::LogicalResult matchAndRewrite(
-        plier::PyCallOp op, mlir::PatternRewriter &rewriter) const override
-    {
-        auto operands = op.getOperands();
-        if (operands.empty())
-        {
-            return mlir::failure();
-        }
-        auto func_type = operands[0].getType();
-        if (!func_type.isa<plier::PyType>())
-        {
-            return mlir::failure();
-        }
-        auto name = func_type.cast<plier::PyType>().getName();
-        llvm::SmallVector<mlir::Type, 8> arg_types;
-        llvm::SmallVector<mlir::Value, 8> args;
-        if (name.consume_front("Function(") && name.consume_back(")"))
-        {
-            llvm::copy(llvm::drop_begin(op.getOperandTypes(), 1), std::back_inserter(arg_types));
-            llvm::copy(llvm::drop_begin(op.getOperands(), 1), std::back_inserter(args));
-            // TODO kwargs
-        }
-        else if (name.consume_front("BoundFunction(") && name.consume_back(")"))
-        {
-            auto getattr = mlir::dyn_cast<plier::GetattrOp>(operands[0].getDefiningOp());
-            if (!getattr)
-            {
-                return mlir::failure();
-            }
-            arg_types.push_back(getattr.getOperand().getType());
-            args.push_back(getattr.getOperand());
-            llvm::copy(llvm::drop_begin(op.getOperandTypes(), 1), std::back_inserter(arg_types));
-            llvm::copy(llvm::drop_begin(op.getOperands(), 1), std::back_inserter(args));
-            name = extract_bound_func_name(name);
-            // TODO kwargs
-        }
-        else
-        {
-            return mlir::failure();
-        }
-
-        return resolver(op, name, args, rewriter);
-    }
-
-private:
-    resolver_t resolver;
-};
 
 mlir::LogicalResult numpy_rewrite(
     plier::PyCallOp op, llvm::StringRef name, llvm::ArrayRef<mlir::Value> args,
