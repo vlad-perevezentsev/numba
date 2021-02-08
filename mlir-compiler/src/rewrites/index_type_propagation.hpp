7c26311ac79993edcc6bc7b53db2caede1305938
Ivan Butygin <ivan.butygin@intel.com>
2021-02-08 04:37:28 +0300

    [MLIR] Separate python-agnostic parts into lib (#172)
    * reorganize project structure
    
    * move dialect definitions to separate lib
    
    * move file
    
    * hide includes
    
    * move transforms and rewrites to plier project
    
    * remove llvm libs from plier
    
    * add plier namespace
    
    * move pipeline to plier
    
    * add plier namespace
    
    * move compiler infra to plier lib
    
    * plier namespace

--- mlir-compiler/src/rewrites/index_type_propagation.hpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-namespace mlir
-{
-class OwningRewritePatternList;
-class MLIRContext;
-}
-
-void populate_index_propagate_patterns(mlir::MLIRContext& context, mlir::OwningRewritePatternList& patterns);
